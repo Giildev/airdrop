51462a7fbce42a9bb8cf0429a847da93b319f30b
Giildev <gilberto.marcano.dev@gmail.com>
2018-07-09 23:28:54 -0400

    folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Dependencies
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// Components & Containers
+import "./App.css";
+import Home from "./containers/Home/";
+import NotFound from "./containers/NotFound";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path={"/"} component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ client/src/containers/Home/index.jsx
@@ -0,0 +1,15 @@
+// Dependencies
+import React, { Component } from "react";
+
+// Components & Containers
+import "./style.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>Here</p>
+      </div>
+    );
+  }
+}
