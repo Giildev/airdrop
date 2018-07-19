9818fe17a42f83300e152e81073bbc1d680e4357
Giildev <gilberto.marcano.dev@gmail.com>
2018-07-19 03:32:36 -0400

    footer

--- client/src/containers/Home/index.jsx
+++ client/src/containers/Home/index.jsx
@@ -8,6 +8,7 @@ import Banner from "../Banner";
 import HowItWorks from "../HowItWorks";
 import FundsRaised from "../FundsRaised";
 import Quote from "../Quote";
+import Footer from "../Footer";
 import AdminSite from "../../components/Admin/Site";
 
 // Styles
@@ -47,6 +48,8 @@ export default class Home extends Component {
         <HowItWorks />
         <FundsRaised />
         <Quote />
+
+        <Footer />
         {/*<h1>
           ANDO VIENDO COMO ES LA VAINA CON LOS TABS PARA EL ADMIN SI TIENES UN
           MEJOR CSS FUEGO XD
