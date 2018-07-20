9ff0d524f4f67201a8fd3a0babfecf912aee4e59
Andres Rodriguez <zplaguee@gmail.com>
2018-07-20 01:15:22 -0400

    Containers Creation

---
+++ client/src/containers/Donate/index.jsx
@@ -0,0 +1,15 @@
+// Dependencies
+import React, { Component } from 'react'
+
+// Components & Containers
+import "./style.css";
+
+export default class Donate extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
