8153f13fb2b7f43ac18f26bee691f7ea4142b63f
mauricio <mamv.2137@gmail.com>
2018-07-20 22:48:58 -0400

    add smooth scroll to Contant + Quote

--- client/src/containers/Contact/index.jsx
+++ client/src/containers/Contact/index.jsx
@@ -6,17 +6,13 @@ import "./style.css";
 
 export default class Contact extends Component {
   render() {
-    return (
-      <section className="contact" id="Contant">
+    return <section className="contact" id="Contact">
         <div className="contact__title">Contact Us</div>
         <div className="contact__subTitle">
-          Please contact{" "}
-          <span className="contact__subTitle__color">
+          Please contact <span className="contact__subTitle__color">
             donate@airdropvenezuela.org
-          </span>{" "}
-          for any questions or requests.
+          </span> for any questions or requests.
         </div>
-      </section>
-    );
+      </section>;
   }
 }
--- client/src/containers/Quote/index.jsx
+++ client/src/containers/Quote/index.jsx
@@ -6,8 +6,7 @@ import "./style.css";
 
 export default class componentName extends Component {
   render() {
-    return (
-      <section className="quote">
+    return <section className="quote" id="About">
         <div className="quote__text">
           <div className="quote__text__quote">
             “A people that loves freedom will in the end be free.”
@@ -21,55 +20,41 @@ export default class componentName extends Component {
           <div className="quote__container__rightCol">
             <p>
               Venezuela should be among the most prosperous nations. Its oil
-              reserves are estimated to be the largest in the world. It has a
-              young and well-educated population and a dynamic culture founded
-              in principles of freedom and fearless opposition to tyranny.
+              reserves are estimated to be the largest in the world. It has
+              a young and well-educated population and a dynamic culture
+              founded in principles of freedom and fearless opposition to
+              tyranny.
             </p>
             <p>
               Today Venezuela has a chaotic and isolated economy, plagued by
               hyperinflation, food shortages, international sanctions, and
               oppression of political dissent. Due to artificially-rigged
-              government exchange rates, the banks in Venezuela are not used for
-              remittance, payments, or donations. Venezuela needs better money.
-              Venezuela needs cryptocurrency.
+              government exchange rates, the banks in Venezuela are not used
+              for remittance, payments, or donations. Venezuela needs better
+              money. Venezuela needs cryptocurrency.
             </p>
             <p>
               Airdrop Venezuela’s mission is to send $1,000,000 in
               crypto-connected digital money to 100,000 ID-authenticated
               Venezuelans, distributing $10 each straight to their AirTM
-              e-wallet. Ten dollars can help a family purchase food, medicine,
-              and scarce imported goods. Access to digital money can help
-              introduce Venezuelans to cryptocurrencies, online freelancer
-              platforms, ecommerce, investments, donations and other income
-              generating web-based opportunities.
+              e-wallet. Ten dollars can help a family purchase food,
+              medicine, and scarce imported goods. Access to digital money
+              can help introduce Venezuelans to cryptocurrencies, online
+              freelancer platforms, ecommerce, investments, donations and
+              other income generating web-based opportunities.
             </p>
             <p>
-              Airdrop Venezuela is powered by{" "}
-              <a
-                href="http://airtm.io"
-                className="quote__container__rightCol__color"
-              >
+              Airdrop Venezuela is powered by <a href="http://airtm.io" className="quote__container__rightCol__color">
                 AirTM
-              </a>, an e-wallet connected to bank networks and e-money systems
-              all over the world (including Venezuela) via direct bank
-              connections and a peer-to-peer network of cryptocurrency buyers
-              and sellers.. AirTM is used by thousands of Venezuelans everyday.
-              Over the past three years, AirTM has become expert at
-              authenticating digital identities to prevent fraud.
+              </a>, an e-wallet connected to bank networks and e-money systems all over the world (including Venezuela) via direct bank connections and a peer-to-peer network of cryptocurrency buyers and sellers.. AirTM is used by thousands of Venezuelans everyday. Over the past three years, AirTM has become expert at authenticating digital identities to prevent fraud.
             </p>
             <p>
-              <a
-                href="http://airtm.io"
-                className="quote__container__rightCol__color"
-              >
+              <a href="http://airtm.io" className="quote__container__rightCol__color">
                 Join AirTM
-              </a>{" "}
-              to give Venezuelans access to digital cash that can be withdrawn
-              as local money (Bolívares) at free market rates.
+              </a> to give Venezuelans access to digital cash that can be withdrawn as local money (Bolívares) at free market rates.
             </p>
           </div>
         </div>
-      </section>
-    );
+      </section>;
   }
 }
