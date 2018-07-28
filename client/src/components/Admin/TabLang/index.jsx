bbe30efbbe2afdad36a5cd081ec86aa70cf086cd
Andres Rodriguez <zplaguee@gmail.com>
2018-07-28 15:17:13 -0400

    Preparing tabLang

--- client/src/components/Admin/TabLang/index.jsx
+++ client/src/components/Admin/TabLang/index.jsx
@@ -6,19 +6,10 @@ import "./style.css";
 
 export default class TabLang extends Component {
   render() {
-    return (
-      <div>
-         <div className="langContainer">
-          <div className="flagContainer">
-            <img src="/spain.png" alt="spanish" className="flagContainer__ico"/>
-            <p className="flagContainer__name">Spanish</p>
-          </div>
-          <div className="flagContainer">
-            <img src="/usa.png" alt="english" className="flagContainer__ico"/>
-            <p className="flagContainer__name">English</p>
-          </div>
-         </div> 
-      </div>
-    )
+    return <div>
+        <div className="langContainer">
+          
+        </div>
+      </div>;
   }
 }
