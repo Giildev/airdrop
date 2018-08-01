65932ea76ee49ca9e836420de786b1c50afe49e0
dashiuz <josuag2@gmail.com>
2018-07-31 21:11:15 -0400

    working in homepage

--- client/src/containers/Faq/index.jsx
+++ client/src/containers/Faq/index.jsx
@@ -6,9 +6,13 @@ import "./style.css";
 
 export default class Faq extends Component {
 
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
     this.acc = document.getElementsByClassName("containerFaq__open__header");
+
+    this.state = {
+      faqs: props.faqs || "question"      
+    }
   }
 
   toggleAccordion = () => {
@@ -28,18 +32,21 @@ export default class Faq extends Component {
   }
 
   render() {
+    const { faqs } = this.state;
+    console.log("PROPS_DATA_FAQS:", this.props.faqs);
     return (
       <div id="FAQ">
         <div className="containerFaq">
           <div className="containerFaq__title">FAQ</div>
           <div className="containerFaq__open">
+            {}
             <div className="containerFaq__open__header" onClick={this.toggleAccordion}>
-              <h4 className="containerFaq__open__header__title"> What is AirTM?</h4>
+              <h4 className="containerFaq__open__header__title"> { `${faqs.question}` }</h4>
               <img src="/faqArrow.png" alt="Arrow"/>
             </div>
             <div className="containerFaq__open__content">
               <p className="containerFaq__open__content__text">
-                AirTM is a digital wallet connected to banks and blockchains, both directly and via a peer-to-peer exchange. AirTM’s provides globally-connected financial services to consumers and businesses in the developing world, including remittance, payments, donations,as well as access to global ecommerce and cryptocurrency. AirTM empowers people in countries with devaluing currencies, hyperinflation,and limited banking systems to exchange local money for digital currency at a free market rate.
+              { `${faqs.answer}` }
               </p>
             </div>
           </div>
--- client/src/containers/Home/index.jsx
+++ client/src/containers/Home/index.jsx
@@ -19,7 +19,7 @@ import AdminSite from "../../components/Admin/Site";
 
 // Styles
 import "./style.css";
-import Loader from "../../components/Loader"
+import Loader from "../../components/Loader";
 
 export default class Home extends Component {
   constructor(props) {
@@ -32,38 +32,56 @@ export default class Home extends Component {
   }
 
   componentDidMount = () => {
-    this.getContentData();
+    this.getContentData();    
   };
 
   getContentData = () => {
-    axios
-      .get(`${config.BASE_URL}/site/${this.state.selectedLan}`)
-      .then(res =>{
-        console.log(res.data.site)
-        this.setState({ content: res.data.site, lan: this.state.selectedLan })
-      }); // fix
+    axios.get(`${config.BASE_URL}/site/${this.state.selectedLan}`).then(res => {
+      console.log("DATA_CONTENT: ",res.data.site);
+      console.log("FAQS_DATA:", res.data.site.faqs[1].question);
+      this.setState({ content: res.data.site, lan: this.state.selectedLan });
+    }); // fix
   };
 
   handleLanguage = e => {
     this.setState({ selectedLan: e.target });
-  };middleSection
-
+  };
+  middleSection;
+  
   render() {
     const { content, selectedLan } = this.state;
-    console.log(content)
-    return content === undefined ? <Loader /> : <div>
+    console.log(content);
+    return content === undefined ? (
+      <Loader />
+    ) : (
+      <div>
         <Header />
-        <Banner image={content.banner} title={content.header[selectedLan].title} description={content.header[selectedLan].description} />
-        <HowItWorks title={content.middleSection[selectedLan].title} description={content.middleSection[selectedLan].description} />
+        <Banner
+          image={content.banner}
+          title={content.header[selectedLan].title}
+          description={content.header[selectedLan].description}
+        />
+        <HowItWorks
+          title={content.middleSection[selectedLan].title}
+          description={content.middleSection[selectedLan].description}
+        />
         <FundsRaised />
         <Quote />
         <TimeLine />
         <Stories />
         <Donate />
-        <MailList title={content.mail[selectedLan].title} description={content.mail[selectedLan].description} />
-        <Faq />
+        <MailList
+          title={content.mail[selectedLan].title}
+          description={content.mail[selectedLan].description}
+        />
+        <Faq 
+          title = {content.faq[selectedLan].title}
+          description = {content.faq[selectedLan].description}
+          faqs = {content.faqs}
+        />
         <Contact />
         <Footer />
-      </div>;
+      </div>
+    );
   }
 }
