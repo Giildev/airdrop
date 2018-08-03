998071ff7bb2f93adee3f9eb42a1efd88c32ef2f
Gilberto Marcano <gilberto.marcano.dev@gmail.com>
2018-08-02 21:26:40 -0400

    css fixs

--- client/src/containers/StorieDetail/index.jsx
+++ client/src/containers/StorieDetail/index.jsx
@@ -1,5 +1,5 @@
 // Dependencies
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
 import config from "../../libs/config";
 
@@ -11,50 +11,51 @@ import Header from "../Header";
 import Footer from "../Footer";
 import Carousel from "../Slider";
 
-
 export default class storieDetail extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
       stories: undefined,
       filteredStories: undefined,
       index: 0,
       selectedLan: props.lan || "EN"
     };
-  };
-
-  componentDidMount = () => {
-    const { location } = this.props
-    this.getStories().then(() => {      
-      const stories = this.state.filteredStories;      
-      if(location.state !== undefined) {
-        stories.map((story, i) => {
-          if (story._id === location.state.sid) {
-            this.setState({ index: i })
-          }
-        })
-      }
-
-      
-    })
   }
 
+  componentDidMount = () => {
+    const { location } = this.props;
+    this.getStories().then(() => {
+      const stories = this.state.filteredStories;
+      if (location.state !== undefined) {
+        stories.map((story, i) => {
+          if (story._id === location.state.sid) {
+            this.setState({ index: i });
+          }
+        });
+      }
+    });
+  };
+
   shouldComponentUpdate = (nextProps, nextState) => {
-    const { location } = this.props
-    const { selectedLan } = this.state
-    if(location.hash !== nextProps.location.hash) {
+    const { location } = this.props;
+    const { selectedLan } = this.state;
+    if (location.hash !== nextProps.location.hash) {
       let title = nextProps.location.hash;
-      title = title.slice(1).split("-").join(" ").toLowerCase();
+      title = title
+        .slice(1)
+        .split("-")
+        .join(" ")
+        .toLowerCase();
 
       const stories = this.state.filteredStories;
       stories.map((story, i) => {
         if (story.title.toLowerCase() === title) {
           this.setState({ index: i }, () => {
             this.scrollTop();
-          })
+          });
         }
-      })
+      });
     }
     return true;
   };
@@ -64,54 +65,54 @@ export default class storieDetail extends Component {
       .get(`${config.BASE_URL}/story`, this.headers)
       .then(res => {
         if (res.status === 200) {
-          this.setState({ stories: res.data.data, filteredStories: this.handleStories(res.data.data) });
+          this.setState({
+            stories: res.data.data,
+            filteredStories: this.handleStories(res.data.data)
+          });
         }
       })
       .catch(err => console.log(err));
-  }
+  };
 
   handleStories = (stories = this.state.stories) => {
-    const { selectedLan } = this.state
+    const { selectedLan } = this.state;
     return stories.filter(story => story.lan === selectedLan);
-  }
+  };
 
   scrollTop = () => {
     document.body.scrollTop = 0; // for safari
     document.documentElement.scrollTop = 0; // for the rest of browsers
-  }
+  };
 
   handleLanguage = lan => {
     this.setState({ selectedLan: lan.toUpperCase() }, () => {
-      this.setState({ filteredStories: this.handleStories() })
+      this.setState({ filteredStories: this.handleStories() });
     });
   };
-  
+
   render() {
     const { index, filteredStories } = this.state;
-    const stories = filteredStories; 
+    const stories = filteredStories;
     return (
       <div>
         <Header handleLanguage={this.handleLanguage} />
-        {
-          stories === undefined 
-          ? <Loader />
-          : (
-            <React.Fragment>
-              <Carousel stories={stories} index={index}/>
-              <div className="storieDetailContainer">
-                <div className="storieDetailContainer__title">More Stories</div>
-                <div className="storieDetailContainer__cardsContainer">
-                {
-                  // stories.map(story => <StorieCard key={story._id} story={story} />)
-                }
-                </div>
-                <button className="storiesSection__button">More Stories</button>
+        {stories === undefined ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            <Carousel stories={stories} index={index} />
+            <div className="storieDetailContainer">
+              <div className="storieDetailContainer__title">More Stories</div>
+              <div className="storieDetailContainer__cardsContainer">
+                {stories.map(story => (
+                  <StorieCard key={story._id} story={story} />
+                ))}
               </div>
-            </React.Fragment>
-          )
-        }
+            </div>
+          </React.Fragment>
+        )}
         <Footer />
       </div>
-    )
+    );
   }
 }
